5ff7fa83a27aa00d80b5c04ea70c5be4ca961ef1
Mat-Merdock <jav.minamo.a01@gmail.com>
2025-03-04 13:17:39 +0530

    Update asian.js

--- asian.js
+++ asian.js
@@ -94,4 +94,107 @@ videoData.push(
         category: 'All,Asian',
         tags: 'ai xi'
     }
+    {
+        url: 'https://javct.net/v/start-245-rm',
+        title: 'START-245-RM ""Always having sex with MINAMO"" A big dick is inserted all day long from morning to night, and it's instant sex',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2025/02/1start245pl.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://pics.dmm.co.jp/mono/movie/adult/1start245/1start245pl.jpg',
+        title: 'START-224-RM Verification. Contacting A Female Teacher Who Assaulted A Student! What Happens To A Woman's Sexual Desire After Three Years Of Abstinence? MINAMO',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2025/01/1start224pl.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://javct.net/v/start-208-rm',
+        title: 'START-208-RM A Men's Beauty Salon Girl Who Is A Kisser. Immediately After Ejaculation, She Gives A Sexy Kiss To Recover The Erection And Ejaculates Continuously For A Total Of 10 Shots MINAMO',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2024/12/1start208pl.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://javct.net/v/mbrbn-049',
+        title: 'MBRBN-049 Hot Spring Beauty/MINAMO',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2024/11/1start189pl.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://javct.net/v/start-189-rm',
+        title: 'START-189-RM MINAMO Junction R18 Version 24-hour Stay-at-home Sex With Unlimited Ejaculation OK, 9 Ejaculations In Total, The Biggest Raw Sex In MINAMO History Released',
+        thumbnail: 'https://pics.dmm.co.jp/mono/movie/adult/1start189/1start189pl.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://javct.net/v/start-158-rm',
+        title: 'START-158-RM Sex Outpatient Clinic Specializing In Sexual Desire Treatment 23 Special Edition SODstar MINAMO Absolute Ace Nurse's Beautiful Big Butt Grabs, Hard Piston Hits Sexual Intercourse Treatment',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2024/10/1start158pl.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://javct.net/v/start-148-rm',
+        title: 'START-148-RM A Beautiful Teacher At My Son's Nursery School And His Wife Had A Secret Meeting At Home While They Were Returning Home And Had Sex For A Week. MINAMO',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2024/09/1start148pl.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://javct.net/v/start-120-rm',
+        title: 'START-120-RM Kakin Ladder SEX MINAMO Where An Unparalleled Beauty Takes Her Around The City At Night And Fucks Her 12 Times Until The Sun Rises',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2024/08/1start120pl.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://javct.net/v/start-107-rm',
+        title: 'START-107-RM A Married Career Woman Who Was Forced To Drink By Her Boss At A Power Harassment Hot Spring Entertainment On A Business Trip And Received A Total Of 21 Sperm Shots MINAMO',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2024/07/1start107pl.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://javct.net/v/start-088-rm',
+        title: 'START-088-RM I Was Seduced By A Super Hot Lounge Lady And I Was Inside The Store And Started Pretending To Be Rational And Had Raw Creampie MINAMO',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2024/06/1start088pl.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://javct.net/v/start-073-rm',
+        title: 'START-073-RM MINAMO ZOMBIE AV DEBUT 3rd Anniversary Work',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2024/05/1start073pl.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://javct.net/v/start-035-rm',
+        title: 'START-035-RM A Relaxing Weekend Meal That Feels Good, Eat, Have Sex, And Sleep MINAMO',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2024/04/START035.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    },
+
+    {
+        url: 'https://javct.net/v/start-022-rm',
+        title: 'START-022-RM Date×××. I Was Attacked By Guys From The Same University's Movie Club That I Trusted, And They Deep Throated Me Over And Over Again, And Then Creampied Me... MINAMO',
+        thumbnail: 'https://cdn.javsts.com/wp-content/uploads/2024/04/START-022.jpg',
+        category: 'All,JAV',
+        tags: 'Minamo'
+    }
 );
